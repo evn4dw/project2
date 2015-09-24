4da0fef15d6bbebe0701b9bc933fa3e8fb783c6c
os <os@debian.hsd1.ut.comcast.net>
2015-09-23 20:20:03 -0600

    Starting pipes,forking

--- shell.cpp
+++ shell.cpp
@@ -28,6 +28,32 @@ bool verify_token_group(string s) {
   return true;
 }
 
+bool verify_token_order(vector<string> &v) {
+  bool contains_pipe = false;
+  bool contains_input_redirection = false;
+  bool contains_output_redirection = false;
+
+  for (int i = 0; i < v.size(); i++) {
+    if (contains_output_redirection && v.at(i).at(0) == INPUT_REDIRECTION)
+      return false;
+    if (contains_output_redirection && v.at(i).at(0) == OUTPUT_REDIRECTION)
+      return false;
+    if (contains_input_redirection && v.at(i).at(0) == INPUT_REDIRECTION)
+      return false;
+
+    if (v.at(i).at(0) == PIPE)
+      contains_pipe = true;
+    if (v.at(i).at(0) == INPUT_REDIRECTION)
+      contains_input_redirection = true;
+    if (v.at(i).at(0) == OUTPUT_REDIRECTION)
+      contains_output_redirection = true;
+    
+    if (contains_pipe && (contains_input_redirection || contains_output_redirection))
+      return false;
+  }
+  return true;
+}
+
 string trim_token_group(string s){
   s = s.substr(s.find_first_not_of(" \f\n\r\t\v"));
   s = s.substr(0, s.find_last_not_of(" \f\n\r\t\v")+1);
@@ -49,13 +75,17 @@ int process_input(string s, vector<string> &token_groups){
   token_groups.resize(0);
 
   if (s.size() == 0) {
-    cout << "Error: input contains no characters.";
+    //If empty, process next line of input without error message
     return -1;
   }
 
   if (s.size() > 100) {
     cout << "Error: input contains more than 100 characters.";
     return -1;
+  }
+
+  if (trim_token_group(s) == "exit") {
+    return -2;
   }
 
   int pos = 0;
@@ -71,22 +101,78 @@ int process_input(string s, vector<string> &token_groups){
 	pos = i+2;
        }
       else {
-	cout<< "Error: input contains invalid character.";
+	cout << "Error: input contains invalid character.";
 	return -1;
       }
     }
   }
   token_groups.push_back(trim_token_group(s.substr(pos)));
+  if (!verify_token_order(token_groups)) {
+      cout << "Error: invalid pipe or redirection tokens." << endl;
+      return -1;
+    }
   return 1; 
 }
 
+int execute_input(vector<string> &v)
+{
+  int pos = 1;
+  while (pos != v.size() - 1) {
+    if (v.at(pos).at(0) == PIPE) {
+      int pipefd[2];
+      
+      //Set up pipe
+    }
+	
+    int pid = fork();
+    
+    /* Child process */
+    if (pid == 0) {
+      if (v.at(pos).at(0) == PIPE) {
+	int pipefd[2];
+	//	char * args[] = {"", "",  };
+	//	char * 
+
+	//	  pipe(pipefd);
+      }
+      else if (v.at(pos).at(0) == OUTPUT_REDIRECTION) {
+
+      }
+      else if (v.at(pos).at(0) == INPUT_REDIRECTION) {
+
+      }
+    }
+    /* Parent process */
+    else if (pid > 0) {
+      
+      
+      
+    }
+    else {
+      cerr << "Error: Invalid pid." << endl;
+      return -2;
+    }
+    
+  }
+}
+  
 int main()
 {
   string input;
   vector<string> token_groups;
-  cout << "shell> ";
+
+  char * buffer = new char[100];
+  string cwd = getcwd(buffer, 100);
+  cout << cwd;
   while (getline(cin, input)) {
-    process_input(input, token_groups);
+    int exit_code = process_input(input, token_groups);
+    if (exit_code == -2)
+      break;
+    else
+      int completed = execute_input(token_groups);
+
+
+    // Testing
     for(int i = 0; i < token_groups.size(); i++)
       cout << token_groups.at(i) << endl;
     cout << "\nshell> ";
